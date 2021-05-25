109102688e39179fa0ae2ac26265cd2bcfad1d93
amxndam <amanmoy@gmail.com>
2021-05-25 12:15:34 +0200

    agujeros

--- codigo/MyScene.js
+++ codigo/MyScene.js
@@ -61,19 +61,49 @@ class MyScene extends THREE.Scene {
     // Debe orbitar con respecto al punto de mira de la cámara
     this.cameraControl.target = look;
   }
+
   
   createGround () {
-    // El suelo es un Mesh, necesita una geometría y un material.
     
-    // La geometría es una caja con muy poca altura
+    // La geometría es una caja con poca altura y 5 agujeros
+    var cilindro1 = new THREE.CylinderGeometry(5,5,1.8,20);
+    var cilindro2 = new THREE.CylinderGeometry(5,5,1.8,20);
+    var cilindro3 = new THREE.CylinderGeometry(5,5,1.8,20);
+    var cilindro4 = new THREE.CylinderGeometry(5,5,1.8,20);
+    var cilindro5 = new THREE.CylinderGeometry(5,5,1.8,20);
+
     var geometryGround = new THREE.BoxGeometry (100,1,50);
+
+    cilindro1.translate(-35,0.2,-11);
+    cilindro2.translate(-1,0.2,-11);
+    cilindro3.translate(33,0.2,-11);
+    cilindro4.translate(-18,0.2,11);
+    cilindro5.translate(17,0.2,11);
+
+    var geometryGroundBSP = new ThreeBSP(geometryGround);
+    var cilindroBSP1 = new ThreeBSP(cilindro1);
+    var cilindroBSP2 = new ThreeBSP(cilindro2);
+    var cilindroBSP3 = new ThreeBSP(cilindro3);
+    var cilindroBSP4 = new ThreeBSP(cilindro4);
+    var cilindroBSP5 = new ThreeBSP(cilindro5);
+
+    var finalGeometryGround = geometryGroundBSP.subtract(cilindroBSP1);
+    var finalGeometryGround2 = finalGeometryGround.subtract(cilindroBSP2);
+    var finalGeometryGround3 = finalGeometryGround2.subtract(cilindroBSP3);
+    var finalGeometryGround4 = finalGeometryGround3.subtract(cilindroBSP4);
+    var finalGeometryGround5 = finalGeometryGround4.subtract(cilindroBSP5);
+
     
     // El material se hará con una textura de madera
     var texture = new THREE.TextureLoader().load('../imgs/ladrillo-mapaNormal.jpg');
     var materialGround = new THREE.MeshPhongMaterial ({color: 0xD3B8F3});
     
     // Ya se puede construir el Mesh
-    var ground = new THREE.Mesh (geometryGround, materialGround);
+    var ground = finalGeometryGround5.toMesh(materialGround);
+    ground.geometry.computeFaceNormals();
+    ground.geometry.computeVertexNormals();
+
+
     
     // Todas las figuras se crean centradas en el origen.
     // El suelo lo bajamos la mitad de su altura para que el origen del mundo se quede en su lado superior
